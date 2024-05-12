efb827bc87471eb30d68bfbb8ddbbdb52b2686fb
Shambo Chowdhury <54593764+typhonshambo@users.noreply.github.com>
2024-05-13 00:34:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,7 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 from pipelines.deployment_pipeline import prediction_service_loader
-from run_deployment import main
-
+from run_deployment import main as run_main
 
 def main():
     st.title("End to End Customer Satisfaction Pipeline with ZenML")
